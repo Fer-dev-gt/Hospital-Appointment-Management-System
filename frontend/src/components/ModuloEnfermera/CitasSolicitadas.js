cf0bc1b35c97dc3de76d468c06038bd474524100
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-10-25 16:41:55 -0600

    Changes appointment status when a valid doctor is selected and remove appointment from nurses module

--- frontend/src/components/ModuloEnfermera/CitasSolicitadas.js
+++ frontend/src/components/ModuloEnfermera/CitasSolicitadas.js
@@ -10,24 +10,65 @@ function CitasSolicitadas() {
     usuarioLoggeadoEnfermera,
   } = React.useContext(HospitalContext);
 
+  const [selectedDoctor, setSelectedDoctor] = React.useState('');
+  const [mostrarDoctores, setMostrarDoctores] = React.useState(false);
+  const citasPendientes = citas.filter(cita => (cita.status !== "confirmar" && cita.status !== "rechazado") || cita.doctor == 'no asignado');
+
   React.useEffect(() => {
     console.log('Lista de citas en Citas.bin');
     getCitas();
     console.log('usuarioLoggeadoEnfermera', usuarioLoggeadoEnfermera);
-  }, [])
+  }, [])  
   
-  const [selectedDoctor, setSelectedDoctor] = React.useState('');
-  const [mostrarDoctores, setMostrarDoctores] = React.useState(false);
+  React.useEffect(() => {
+    console.log('selectedDoctor2:', selectedDoctor);
+  }, [selectedDoctor]);
   
-  const citasPendientes = citas.filter(cita => cita.status !== "conrirmar" && cita.status !== "rechazado");
 
-  const aceptarCita = (cita) => {
+  const subirCitaAlArchivo = async () => {
+
+  }
+
+
+  const aceptarCita = (cita, doctor="") => {
+    if(cita.status === 'confirmar' && selectedDoctor === '') alert('No se ha seleccionado doctor');
+    if(cita.status === 'confirmar' && selectedDoctor !== '') {
+      console.log('guardando doctor');
+      cita.status = 'aceptada';
+      cita.doctor = selectedDoctor;
+      console.log('Datos de citas actualizadas', citas);
+      setMostrarDoctores(!mostrarDoctores);
+      aceptarCita(cita)
+      return;
+    }
+
+    if(cita.status === 'aceptada') console.log('quitando cita');
+
     if(cita.status !== 'confirmar') {
-      console.log('status 1', cita);
       cita.status = 'confirmar';
-      console.log('status 2', cita);
       setMostrarDoctores(!mostrarDoctores);
     }
+  }
+
+
+  const cancelarCita = (cita) => {
+    if(cita.status !== 'pendiente') {
+      cita.status = 'pendiente';
+      setMostrarDoctores(!mostrarDoctores);
+    }
+  }
+
+
+  const cambiarDoctor = (event) => {
+    if(event.target.value === 'Seleccionar doctor') {
+      const nuevoDoctor = '';
+      setSelectedDoctor(nuevoDoctor)
+      console.log('selectedDoctor: ', nuevoDoctor);
+      return;
+    }
+    const nuevoDoctor = event.target.value;
+    setSelectedDoctor(nuevoDoctor)
+    console.log('selectedDoctor: ', nuevoDoctor);
   }
 
 
@@ -35,7 +76,6 @@ function CitasSolicitadas() {
     <div>
       <h1>Listado de citas pendientes</h1>
       {citasPendientes.length === 0 && <p>No Hay citas por administrar</p>}
-
       {citasPendientes.length > 0 && (
       <table className="two-column-table">
         <thead>
@@ -56,23 +96,25 @@ function CitasSolicitadas() {
                 <td>{cita.motivoCita}</td>
                 
                 <td><button>Rechazar</button></td>
-                <td><button onClick={()=>{aceptarCita(cita)}}>Atender</button> {(cita.status === 'confirmar') && 
-                  <select  onChange={(e) => setSelectedDoctor(e.target.value)}>
-                    <option value={null}>Seleccionar doctor</option>
-                    {usuarios.filter(usuario => usuario.userType === 'doctor').map(doctor => (
-                      <option key={doctor.id} value={doctor.nombre}>{doctor.nombre}</option>
-                    ))}
-                  </select>}
+                <td><button onClick={()=>{aceptarCita(cita)}}>{cita.status!=='confirmar' ? "Aceptar":"Confirmar"}</button> {(cita.status === 'confirmar') && 
+                  (<>
+                    <select onChange={cambiarDoctor}>
+                      <option value={null}>Seleccionar doctor</option>
+                      {usuarios.filter(usuario => usuario.userType === 'doctor').map(doctor => (
+                        <option id={doctor.nombre} key={doctor.id} value={doctor.nombre}  >{doctor.nombre}</option>
+                      ))}
+                    </select>
+                    <button onClick={()=>{cancelarCita(cita)}}>Regresar</button>
+                  </>
+                  )}
                 </td>
               </tr>
             ))
           }
         </tbody>
       </table>)}
-      
     </div>
   )
-
 }
 
 export { CitasSolicitadas };
