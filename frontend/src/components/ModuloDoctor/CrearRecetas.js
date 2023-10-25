9b7eb6f619df258685eed9220b387a16a6dbcd8b
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-10-24 19:32:38 -0600

    Implemented nurse and doctor main page after login

---
+++ frontend/src/components/ModuloDoctor/CrearRecetas.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { HospitalContext } from "../../Context";
+
+const CrearRecetas = () => {
+
+  return(
+    <div>
+      <h1>Modulo Doctor</h1>
+    </div>
+  )
+};
+
+
+export { CrearRecetas };
---
+++ frontend/src/components/ModuloEnfermera/CitasSolicitadas.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { HospitalContext } from "../../Context";
+
+
+function CitasSolicitadas() {
+
+
+  return(
+    <>
+      <h1>Citas Solicitadas</h1>
+    </>
+  )
+
+}
+
+export { CitasSolicitadas };
